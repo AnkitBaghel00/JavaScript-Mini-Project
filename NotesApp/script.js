82d26bb52451309e71ac7a7f1b4dc722bb1211a4
baghe <AnkitBaghel00>
2025-10-04 01:13:45 +0530

    Initial commit

--- NotesApp/script.js
+++ NotesApp/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelector(".input-box")
@@ -40,4 +41,48 @@ document.activeElement("keydown",event => {
         document.execCommand("insertLineBreak");
         event.preventDefault();
     }
+=======
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelector(".input-box")
+
+function shoaNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+shoaNotes();
+
+function updateStorage(){
+    localStorage.setItem("notes",notesContainer.innerHTML)
+}
+
+createBtn.addEventListener("click",()=>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable","true");
+    img.src = "images/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+});
+
+notesContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt =>{
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
+})
+
+document.activeElement("keydown",event => {
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+>>>>>>> 8b9fd8a (Initial commit)
 })
